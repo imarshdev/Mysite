dc3eace9b9324310ff925d074524a93930834e7b
forevermarsh <marshmansur004@gmail.com>
2024-02-07 13:52:03 +0300

    i keep making changes

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -2,17 +2,25 @@ import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
 import { Link } from "react-router-dom";
 import "../index.css";
-import { Spotify, Linkedin, Discord, Github, Imdb, Instagram, TwitterX, Snapchat  } from "../components/svg";
-
+import {
+  Spotify,
+  Linkedin,
+  Discord,
+  Github,
+  Imdb,
+  Instagram,
+  TwitterX,
+  Snapchat,
+} from "../components/svg";
 
 function Contacts() {
-  useEffect(()=>{
+  useEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
-  },[]);
+  }, []);
 
   return (
     <div
@@ -26,37 +34,56 @@ function Contacts() {
     >
       <Navbar />
       <h1>Contacts</h1>
-      <div style={{display: 'flex', flexWrap: 'wrap', width: '100%', justifyContent: 'center'}}>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+          justifyContent: "center",
+        }}
+      >
         <span className="skill-span">
-        <Spotify />
-        <em className="language">Spotify</em>
+          <a className="Link" href="https://open.spotify.com/user/sgw331agtxe12j35fd5jrjfwb?si=lraPTE8-RJiXJ5EnHntdbA">
+          <Spotify />
+          <em className="language">Spotify</em>
+          </a>
         </span>
 
         <span className="skill-span">
-        <Linkedin />
-        <em className="language">LinkedIn</em>
+          <a className="Link" href="">
+            <Linkedin />
+            <em className="language">LinkedIn</em>
+          </a>
+        </span>
+
+
+        <span className="skill-span">
+        <a className="Link" href="https://discordapp.com/users/1137663822828683295">
+          <Discord />
+          <em className="language">Discord</em>
+          </a>
         </span>
 
         <span className="skill-span">
-        <Discord />
-        <em className="language">Discord</em>
+          <a className="Link" href="https://github.com/forevermarsh">
+          <Github />
+          <em className="language">Github</em>
+          </a>
         </span>
 
         <span className="skill-span">
-        <Github />
-        <em className="language">Github</em>
+          <a className="Link" href="https://x.com/eternallyMarsh">
+          <TwitterX />
+          <em className="language">Twitter</em>
+          </a>
         </span>
 
         <span className="skill-span">
-        <TwitterX />
-        <em className="language">Twitter</em>
+          <a className="Link" href="https://instagram.com/forever.marsh">
+          <Instagram />
+          <em className="language">Instagram</em>
+          </a>
         </span>
-
-        <span className="skill-span">
-        <Instagram />
-        <em className="language">Instagram</em>
-        </span>
-
       </div>
     </div>
   );
