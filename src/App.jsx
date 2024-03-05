da98fc6e419459824cdbfad8170a9c9b99d6c2f2
forevermarsh <marshmansur004@gmail.com>
2024-03-05 17:09:54 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// hi, this is my personnal site in react
+// i had react-router-dom because i had multiple pages but now i just want to use one page
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/about";
-import Notes from "./pages/notes";
-import Projects from "./pages/projects";
-import Contacts from "./pages/contacts";
-import Play from "./pages/play";
 
 function App() {
-
-
   return (
-    <div style={{width: '100%'}}>
+    <div style={{ width: "100%" }}>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/about" element={<About />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/play" element={<Play />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+// this i sthe main page and evidently the only page
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import clickSound from "../click_sound.mp3";
 import "../index.css";
-import About from "./about";
+// i use svg icons because they are easier and don't slow your site
 import {
   Spotify,
   Linkedin,
@@ -12,9 +13,24 @@ import {
   TwitterX,
   Snapchat,
   Email,
+  Html,
+  Css,
+  Javascript,
+  Java,
+  Python,
+  CPlusPlus,
+  ReactJs,
+  Vuejs,
+  TailwindCss,
+  Angular,
+  Git,
+  VscodeIcon,
+  Letterboxd,
 } from "../components/svg";
 
+// main component "home"
 function Home() {
+  // this was usefull but not quite anymore, i use useEffect to prevent react-router-dom from accessing a new page fro, the exact point the previous page was at
   useEffect(() => {
     window.scrollTo({
       top: 0,
@@ -22,12 +38,14 @@ function Home() {
       behavior: "smooth",
     });
   }, []);
+  // not entirely usefull but whatever
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleMode = () => {
     setIsDarkMode(!isDarkMode);
   };
 
+  // the click sound you hear
   function play() {
     new Audio(clickSound).play();
   }
@@ -35,6 +53,15 @@ function Home() {
   const all = () => {
     play();
     toggleMode();
+  };
+  // navigating to sections, i simply don't like uaing the <a> tag!!!
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const blogRef = useRef(null);
+  const meRef = useRef(null);
+
+  const scrollToDiv = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -49,6 +76,7 @@ function Home() {
           backgroundColor: "#fff",
         }}
       >
+        {/* the topper with my name */}
         <div
           style={{
             width: "100%",
@@ -58,13 +86,16 @@ function Home() {
             justifyContent: "space-around",
           }}
         >
-          <h1 style={{fontWeight: 'bolder', color:'#152b42', fontSize: '3rem'}}>Eternally <em style={{}}>Marsh</em></h1>
+          <h1
+            style={{ fontWeight: "bolder", color: "#152b42", fontSize: "3rem" }}
+          >
+            Eternally <em style={{}}>Marsh</em>
+          </h1>
         </div>
 
+        {/* topper with description */}
         <div className="topper">
-          <div className="picture">
-
-          </div>
+          <div className="picture"></div>
           <div style={{ width: "65%" }}>
             <h4 style={{ padding: "20px" }}>Senior Front-End Developer.</h4>
             <p style={{ padding: "0 20px" }}>
@@ -73,8 +104,9 @@ function Home() {
           </div>
         </div>
 
+        {/* this are links to sections */}
         <div
-          className={isDarkMode ? "dark" : "light"}
+          className="light"
           style={{
             width: "90%",
             display: "flex",
@@ -86,7 +118,7 @@ function Home() {
           }}
         >
           <div onClick={play} className="link-div">
-            <Link to="/about" className="main-link">
+            <Link onClick={() => scrollToDiv(aboutRef)} className="main-link">
               <button
                 className="main-buttons"
                 style={{
@@ -103,7 +135,10 @@ function Home() {
           </div>
 
           <div onClick={play} className="link-div">
-            <Link to="/projects" className="main-link">
+            <Link
+              onClick={() => scrollToDiv(projectsRef)}
+              className="main-link"
+            >
               <button
                 className="main-buttons"
                 style={{
@@ -112,32 +147,15 @@ function Home() {
               >
                 <em style={{ fontSize: "20px" }}>Projects</em>
                 <p style={{ fontSize: "14px" }}>
-                  Projects with descriptions, screenshots, and links to
-                  GitHub repositories.
+                  Projects with descriptions, screenshots, and links to GitHub
+                  repositories.
                 </p>
               </button>
             </Link>
           </div>
 
           <div onClick={play} className="link-div">
-            <Link to="/notes" className="main-link">
-              <button
-                className="main-buttons"
-                style={{
-                  textAlign: "end",
-                }}
-              >
-                <em style={{ fontSize: "20px" }}>Blog</em>
-                <p style={{ fontSize: "14px" }}>
-                  Posts, articles, notes and PDF books related to front-end
-                  development,
-                </p>
-              </button>
-            </Link>
-          </div>
-
-          <div onClick={play} className="link-div">
-            <Link to="/play" className="main-link">
+            <Link onClick={() => scrollToDiv(meRef)} className="main-link">
               <button
                 className="main-buttons"
                 style={{
@@ -152,8 +170,26 @@ function Home() {
               </button>
             </Link>
           </div>
+
+          <div onClick={play} className="link-div">
+            <Link onClick={() => scrollToDiv(blogRef)} className="main-link">
+              <button
+                className="main-buttons"
+                style={{
+                  textAlign: "end",
+                }}
+              >
+                <em style={{ fontSize: "20px" }}>Blog</em>
+                <p style={{ fontSize: "14px" }}>
+                  Posts, articles, notes and PDF books related to front-end
+                  development,
+                </p>
+              </button>
+            </Link>
+          </div>
         </div>
 
+        {/* random quote */}
         <div
           style={{
             width: "80%",
@@ -170,6 +206,7 @@ function Home() {
           </div>
         </div>
 
+        {/* links to my social media */}
         <div style={{ height: "30vh" }}>
           <div
             style={{
@@ -214,7 +251,285 @@ function Home() {
                 <Email />
               </a>
             </span>
+
+            <span className="skill-span">
+              <a className="Link" href="eternallymarsh04@gmail.com">
+                <Discord />
+              </a>
+            </span>
           </div>
+        </div>
+
+        {/* new section for my About content */}
+        <div
+          ref={aboutRef}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "start",
+            width: "100%",
+            textAlign: "start",
+          }}
+        >
+          <h1 style={{ padding: "0 5%" }}>About Me</h1>
+          <p className="about-me">
+            I'm Marsh Mansur, I'm 21 years old and I'm a front-end developer. I
+            create dynamic and user-friendly websites. Specializing in React.js,
+          </p>
+          <p className="about-me">
+            When I'm not coding, 'which is not often', I like to read books,
+            expecially Greek mythology fantasies, "The Percy Jackson Series, The
+            Song of Achilles, Adriadne and more". I also workout... psych!!.
+            Weekends for me are movie-time so I go to the movies with friends
+            and sometimes alone.
+          </p>
+          <p className="about-me">
+            I just watched Dune part 2, the first movie was amazing but this one
+            is just something else entirely. I bought the six Og dune books and
+            I'm gonna be reading them in the very little extra time I get. Denis
+            Villeneuve just made my top three movie directors and Dune part two
+            just become my secon favourite movie of all time.
+          </p>
+          <p className="about-me">
+            From a young age, music has always been a major part of my life, it
+            has helped me overcome tough situations and put me in tough
+            situations as well. I feel like proper music can be a tune that puts
+            us in proper flow with life and existance in general. When I started
+            coding it was a bit challenging but with the right song playing, I
+            can accomplish anything.
+          </p>
+          <p className="about-me">
+            As a software engineer, some of my major skills include programming
+            languages (such as Java, Javascript, Python, or C++),
+            problem-solving, algorithm design, and software development
+            methodologies. Skills include version control (e.g., Git),
+            debugging, unit testing, and data structures.
+          </p>
+          <h1 style={{ padding: "0 5%" }}>Skills & Tools</h1>
+          <div
+            onClick={play}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              width: "100%",
+            }}
+          >
+            <button className="skill-button">
+              <Html />
+              <em className="language">Html</em>
+            </button>
+
+            <button className="skill-button">
+              <Css />
+              <em className="language">Css</em>
+            </button>
+
+            <button className="skill-button">
+              <Javascript />
+              <em className="language">JavaScript</em>
+            </button>
+
+            <button className="skill-button">
+              <Java />
+              <em className="language">Java</em>
+            </button>
+
+            <button className="skill-button">
+              <Python />
+              <em className="language">Python</em>
+            </button>
+
+            <button className="skill-button">
+              <CPlusPlus />
+              <em className="language">C++</em>
+            </button>
+
+            <button className="skill-button">
+              <ReactJs />
+              <em className="language">React Js</em>
+            </button>
+
+            <button className="skill-button">
+              <Vuejs />
+              <em className="language">Vue Js</em>
+            </button>
+
+            <button className="skill-button">
+              <TailwindCss />
+              <em className="language">TailwindCss</em>
+            </button>
+
+            <button className="skill-button">
+              <Angular />
+              <em className="language">Angular</em>
+            </button>
+
+            <button className="skill-button">
+              <Git />
+              <em className="language">Git</em>
+            </button>
+
+            <button className="skill-button">
+              <VscodeIcon />
+              <em className="language">VS Code</em>
+            </button>
+          </div>
+        </div>
+
+        {/* new section for my projects */}
+        <div
+          ref={projectsRef}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <h1>Projects</h1>
+          <ul>
+            <li>ToDoList WebApp</li>
+            <li>Chat App</li>
+            <li>Music Player</li>
+            <li>Movie Library App</li>
+          </ul>
+        </div>
+
+        {/* new section for more about me */}
+        <div
+          ref={meRef}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <h1>Here's What I love</h1>
+          <p style={{ padding: "0 10%", textAlign: "center" }}>
+            Music has been very necessary in almost all aspects of my life. I
+            listen to music no matter the situation or emotional state. <br />I
+            genuinely can't say what my favourite song is, I have one or two for
+            every specific mood at the time. <br />
+            When i think <em>"favourite + artist"</em> I think <b>Ruth B</b> I
+            listen to the Safe Heaven Album atleast once a day.
+          </p>
+
+          <p style={{ padding: "0 10%", textAlign: "center" }}>
+            Other artists I listen to are Shakira, Kanye West, Megan Washington,
+            Rosa Linn and the one and only Billie Eilish
+          </p>
+
+          <span style={{ width: "40%" }}>
+            <a
+              className="Link"
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "start",
+                textDecoration: "none",
+              }}
+              href="https://open.spotify.com/playlist/7vwApBAR0SqbXmqxrFmrk9?si=vpz1JtYKSzOLeKq46kLMPQ"
+            >
+              <Spotify />
+              <em style={{ paddingLeft: "5%" }}>My Coding Playlist</em>
+            </a>
+          </span>
+
+          <p style={{ padding: "0 10%", textAlign: "center" }}>
+            "Cinema," a creation by Auguste and Luis Lumiere. I love movies and
+            tv, people say it's an escape from reality but it's a visit to the
+            limits of reality. <br />
+            Over my young life, I've come to appreciate the work of directors{" "}
+            <b>
+              Quentin Tarantino, Martin Scorsese, David Fincher and Christopher
+              Nolan
+            </b>
+          </p>
+
+          <span style={{ width: "40%" }}>
+            <a
+              className="Link"
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "start",
+                textDecoration: "none",
+              }}
+              href="https://boxd.it/8LFkD"
+            >
+              <Letterboxd />
+              <em style={{ paddingLeft: "5%" }}>My Letterboxd</em>
+            </a>
+          </span>
+
+          <p style={{ padding: "0 10%" }}>
+            I also love reading books, Cooking & Eating, Lucid dreaming and
+            Writing.
+          </p>
+
+          <p style={{ padding: "0 10%" }}>
+            My Favourite movies are; <br />
+            <ul>
+              <li>Gone Girl</li>
+              <li>Dune Part 2</li>
+              <li>Fight Club</li>
+              <li>Intersteller</li>
+              <li>Beetlejuice</li>
+            </ul>
+          </p>
+
+          <p style={{ padding: "0 10%" }}>
+            My Favourite Animations are; <br />
+            <ul>
+              <li>Rick & Morty</li>
+              <li>Avatar the last Airbender</li>
+              <li>One Piece</li>
+              <li>Death Note</li>
+              <li>Attack on Titan</li>
+            </ul>
+          </p>
+
+          <p style={{ padding: "0 10%" }}>
+            These are the books I read <br />
+            <ul>
+              <li>Dune Series</li>
+              <li>
+                Percy Jackson Series (on repeat) <br /> ~Rick Riordan
+              </li>
+              <li>
+                Ms Dalloway <br /> ~Virginia Woolf
+              </li>
+              <li>
+                Twilight <br /> ~Stephanie Meyer
+              </li>
+              <li>
+                Warm Bodies <br /> ~Isaac Marion
+              </li>
+              <li>
+                The Haunting of Hill House <br /> ~Shirly Jackson
+              </li>
+            </ul>
+          </p>
+        </div>
+
+        {/* new section for my blog content */}
+        <div
+          ref={blogRef}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <h1 style={{ color: "black" }}>Blog</h1>
         </div>
       </div>
     </div>
